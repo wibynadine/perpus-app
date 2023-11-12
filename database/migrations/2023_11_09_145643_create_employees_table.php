d1c5b2492363659f466688c2164de0fb489bb185
wibynadine <wibynadine@gmail.com>
2023-11-12 12:05:15 +0700

    first init github

---
+++ database/migrations/2023_11_09_145643_create_employees_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('emp_name');
+            $table->date('dob');
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+ 
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
